be4a3192b84e07f16ed6f4676e85db2ecc1674f7
codeproduction <7468@cch.edu.pk>
2023-05-09 16:53:04 -0700

    Setting up TypeScript types

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,6 @@ import { getProjects } from "@/sanity/sanity-utils"
 export default async function Home() {
   const projects = await getProjects();
 
-
   return (
     <div>
       {projects.map((project) => (
