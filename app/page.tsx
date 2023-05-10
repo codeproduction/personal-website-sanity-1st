b491b20319b073374edbb83a6d47b00dfba26f7c
codeproduction <7468@cch.edu.pk>
2023-05-09 18:34:00 -0700

    Added image for FIDS project & updated styling

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import { getProjects } from "@/sanity/sanity-utils"
 import Image from "next/image";
+import Link from "next/link";
 
 export default async function Home() {
   const projects = await getProjects();
@@ -20,25 +21,28 @@ export default async function Home() {
       </p>
 
       <h2 className="mt-24 font-bold text-gray-700 text-3xl">My Projects</h2>
-      
+
       <div className="mt-5 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">  {/* changed buttons of projects from column to grid/rows */}
         {projects.map((project) => (
-          <div key={project._id} className="border border-gray-500 rounded-lg p-3">
+          <Link
+            href={`projects/${project.slug}`}
+            key={project._id}
+            className="border-2 border-gray-500 rounded-lg p-1 hover:scale-105 hover:border-blue-500 transition">
             {project.image && (
               <Image
                 src={project.image}
                 alt={project.name}
-                width={250}
-                height={100}
+                width={750}
+                height={300}
                 className="object-cover rounded-lg border border-gray-500"
               />
             )}
 
-            <div className="font-extrabold bg-gradient-to-r from-orange-400 via-red-500 to-purple-600
+            <div className="mt-2 font-extrabold bg-gradient-to-r from-orange-400 via-red-500 to-purple-600
         bg-clip-text text-transparent">
               {project.name}
             </div>
-          </div>
+          </Link>
         ))}
       </div>
     </div>
