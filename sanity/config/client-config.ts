c282cd717214df5db57f207a7a04e250c85ebbc3
codeproduction <7468@cch.edu.pk>
2023-05-09 20:49:02 -0700

    Improved styling, edited layout, and added navbar

---
+++ sanity/config/client-config.ts
@@ -0,0 +1,9 @@
+import exp from "constants";
+
+const config = {
+    projectId: "n1ovu5rt",
+    dataset: "production",
+    apiVersion: "2023-05-08",
+};
+
+export default config;
--- sanity/sanity-utils.ts
+++ sanity/sanity-utils.ts
@@ -1,15 +1,9 @@
 import { Project } from "@/types/Project";
 import { createClient, groq } from "next-sanity";
+import clientConfig from "./config/client-config";
 
 export async function getProjects(): Promise<Project[]> {       
-    const client = createClient({
-        projectId: "n1ovu5rt",
-        dataset: "production",
-        apiVersion: "2023-05-08",
-    });
-
-
-    return client.fetch(
+    return createClient(clientConfig).fetch(
         groq`*[_type == "project"]{
             _id,
             _createdAt,
@@ -19,7 +13,22 @@ export async function getProjects(): Promise<Project[]> {
             url, 
             content
         }`
-    )
+    );
 
 
+}
+
+export async function getProject(slug : string): Promise<Project> {
+    return createClient(clientConfig).fetch(
+        groq`*[_type == "project" && slug.current == $slug][0]{
+            _id,
+            _createdAt,
+            name,
+            "slug": slug.current,
+            "image": image.asset->url,
+            url, 
+            content
+        }`,
+        { slug }
+    );        
 }
